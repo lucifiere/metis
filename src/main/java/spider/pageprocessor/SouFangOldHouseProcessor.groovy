2da0297e6d15242997aafd57f2fab4f3013e3e11
lucifiere <lucifiere@126.com>
2016-12-11 00:15:53 +0800

    增加分析逻辑

--- src/main/java/spider/pageprocessor/SouFangOldHouseProcessor.groovy
+++ src/main/java/spider/pageprocessor/SouFangOldHouseProcessor.groovy
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory
 import spider.constant.Condition
 import spider.constant.Config
 import spider.constant.Pattern
+import spider.service.SpiderService
 import us.codecraft.webmagic.Page
 import us.codecraft.webmagic.ResultItems
 import us.codecraft.webmagic.Site
 import us.codecraft.webmagic.processor.PageProcessor
+import us.codecraft.webmagic.selector.Selectable
 
 /**
  *  Created by Tyler.Wang on 2016/12/10.
@@ -56,16 +58,17 @@ class SouFangOldHouseProcessor extends BasePageProcessor implements PageProcesso
 
     private static void crawlPageInfo(Page page) {
         page.putField('origin', Pattern.ORIGIN_OLD)
-        String baseInfo = page.getHtml().xpath(Pattern.X_M_BASE_INFO).get()
+        String baseInfo = page.getHtml().xpath(Pattern.X_M_BASE_INFO).get().replaceAll('<.*?>', " ")
         page.putField("baseInfo", baseInfo)
-        String courtIntroduce = page.getHtml().xpath(Pattern.X_M_COURT_INTRODUCE).get()
+        String courtIntroduce = page.getHtml().xpath(Pattern.X_M_COURT_INTRODUCE).get().replaceAll('<.*?>', " ")
         page.putField("courtIntroduce", courtIntroduce)
-        String matingInfo = page.getHtml().xpath(Pattern.X_M_MATING_INFO).get()
+        String matingInfo = page.getHtml().xpath(Pattern.X_M_MATING_INFO).get().replaceAll('<.*?>', " ")
         page.putField("matingInfo", matingInfo)
-        String surrounding = page.getHtml().xpath(Pattern.X_M_SURROUNDING).get()
+        String surrounding = page.getHtml().xpath(Pattern.X_M_SURROUNDING).get().replaceAll('<.*?>', " ")
         page.putField("surrounding", surrounding)
-        String traffic = page.getHtml().xpath(Pattern.X_M_TRAFFIC).get()
+        String traffic = page.getHtml().xpath(Pattern.X_M_TRAFFIC).get().replaceAll('<.*?>', " ")
         page.putField("traffic", traffic)
+        SpiderService.analysisInfo(page, [baseInfo, courtIntroduce, matingInfo, surrounding, traffic])
     }
 
     @Override
@@ -77,4 +80,5 @@ class SouFangOldHouseProcessor extends BasePageProcessor implements PageProcesso
         ResultItems res = page.getResultItems()
         return res.get('builN') == null && res.get('baseInfo') == null
     }
+
 }
